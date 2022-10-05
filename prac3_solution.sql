c6b0ab31028801e5ab4d56d8e31573e0278449d0
Black Hole Diver <76709428+black-hole-diver@users.noreply.github.com>
2022-10-05 13:56:55 +0200

    Create prac3_solution.sql

---
+++ prac3_solution.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE PROCEDURE empty_blocks(p_owner VARCHAR2, p_table VARCHAR2) IS
+    reserved_blocks integer := 0;
+    filled_blocks integer;
+    empty_blocks integer := 0;
+    v_str varchar2(2000);
+BEGIN
+    select blocks into reserved_blocks
+        from dba_segments where owner = p_owner and segment_name = p_table;
+    v_str := 'select count(distinct DBMS_ROWID.ROWID_BLOCK_NUMBER(rowid))  from ' || p_owner || '.' || p_table;
+    EXECUTE IMMEDIATE v_str INTO filled_blocks;
+    empty_blocks := reserved_blocks - filled_blocks;
+    DBMS_OUTPUT.PUT_LINE(' Empty Blocks: ' || empty_blocks);
+END;
+
+--test1
+set serveroutput on
+execute empty_blocks('NIKOVITS', 'EMPLOYEES');
